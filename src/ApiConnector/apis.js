8758799b3a5618bf72c60abc7b4c1937fd12b08d
Anubhav <1rn21cs026.anubhavkumar@rnsit.ac.in>
2025-06-02 22:28:03 +0530

    change the signup route

--- src/ApiConnector/apis.js
+++ src/ApiConnector/apis.js
@@ -6,7 +6,7 @@ const API_BASE_URL = 'https://backend-internship-zojh.onrender.com/api/v1';
 
 // Example API endpoints
 export const LOGIN = `${API_BASE_URL}/auth/login`;
-export const SIGNUP = `${API_BASE_URL}/auth/signup`;
+export const SIGNUP = `${API_BASE_URL}/auth/register`;
 export const GETCAHRGER = `${API_BASE_URL}/chargers/`;
 export const DELETECHARGER = `${API_BASE_URL}/chargers/delete-charger`;
 export const UPDATECHARGER = `${API_BASE_URL}/chargers/update-charger`;
